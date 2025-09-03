54251ba20ab2209fd1c798270b674eb3f5ac13cb
xinixxx <0124cmh@naver.com>
2025-09-03 16:13:33 +0900

    DJango 백엔드 및 DB연동 까지

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+	# Django 의 기본 User 모델 (AbstractUser)
+	# 나중에 닉네임, 프로필 사진 등 서비스에서만 필요한 필드를 추가할 때 여기에 작성
+	# 예시) nickname = models.CharField(max_length=50, unique=True, null=True)
+	
+	# 지금은 추가기능 없이 기본기능만 사용하기 위해 pass 사용
+    pass
